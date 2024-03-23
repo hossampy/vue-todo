1147b443e8a68c3dcce615b4a8074ee35fe249cb
hossam <hossameddine.djennad@gmail.com>
2024-03-23 18:09:47 +0100

    add fetch data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-hello
+  <div v-if="projects.length">
+    <div v-for="project in projects" :key="project.id">
+      <p>{{ project.title }}</p>
+    </div>
+  </div>
   </div>
 </template>
 
@@ -12,6 +16,18 @@ export default {
   name: 'HomeView',
   components: {
    
+  },
+  data(){
+    return{
+      projects:[]
+    }
+  }
+  ,
+  mounted(){
+    fetch('http://localhost:3000/projects')
+    .then(res=>res.json())
+    .then(data=>this.projects = data)
+    .catch(err=>console.log(err.message))
   }
 }
 </script>
